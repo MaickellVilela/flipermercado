ce42bc2057f94d0204c2591d1310fb0441ab66fe
Marcos Felipe Eipper <marcos@eipper.com.br>
2019-09-13 14:32:42 -0300

    Fix confirmation styles identation

--- components/confirmation/styles.js
+++ components/confirmation/styles.js
@@ -1,13 +1,13 @@
 const styles = {
- whiteOverlay: {
-     position: 'absolute',
-     left: 0,
-     right: 0,
-     top: 0,
-     bottom: 0,
-     alignItems: 'center',
-     justifyContent: 'center',
- },
+  whiteOverlay: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 }
 
 export default styles
