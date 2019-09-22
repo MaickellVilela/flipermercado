1f002ab423e6df61ac9d0e679debbd48eda8525f
Thaynã B. Moretti <moretty@gmail.com>
2019-09-22 15:22:10 -0300

    Create default theme

---
+++ themes/default.js
@@ -0,0 +1,18 @@
+export default theme = {
+  colors: {
+    primary: '#FC0A7E',
+  },
+  Button: {
+    buttonStyle: {
+      borderRadius: 0,
+    },
+  },
+  ListItem: {
+    containerStyle: {
+      borderBottomColor: '#FC0A7E',
+    },
+  },
+  Avatar: {
+    rounded: true,
+  },
+};
